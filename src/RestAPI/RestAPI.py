9b4c13e0d26e858265cf4a59e2ba32ffdc51dda2
paultownend <paul.townend@umu.se>
2023-08-02 23:57:13 +0200

    Minor updates

--- src/RestAPI/RestAPI.py
+++ src/RestAPI/RestAPI.py
@@ -20,19 +20,13 @@ version = "0.1"
 # Second service gets VM placement request and parses and sends on to AI?  Quicker to go straight to AI orch.
 
 
-# Monitor service - build up 
-
-
-
 from fastapi import Request, FastAPI, BackgroundTasks
-
 theService = FastAPI()
-
 
 
 ###### Version information, accessible via GET request
 
-@theService.get("/about")
+@theService.get("/version")
 async def about():
     return {"COGNIT Monitoring service - Version " + str(version)}
 
@@ -42,12 +36,15 @@ async def about():
 ###### The service returns an "ACK" upon receipt of Json, and then processes the JSON in the background asynchronously 
 
 def processJSON(theJson: str):
-    print("Hello")
-    # do whatever here
-
+    print("Processing JSON")
+    # validate the JSON and reject if necessary
+    # parse JSON
+    # store into memory object (pandas dataframe)
+    # periodically store to disk
+ 
 
 @theService.post("/CognitMonitor")
-async def post_body(jsonPayload: Request):
+async def post_body(jsonPayload: Request, background_tasks: BackgroundTasks):
     background_tasks.add_task(processJSON, jsonPayload)
     return {"ACK"}
 
