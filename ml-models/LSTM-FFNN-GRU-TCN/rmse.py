0f4a5797167cf79edd82992d6222869df01c94c6
Javad <javadforough@gmail.com>
2024-11-07 17:06:58 +0100

    Organize model repository branch for Version 1.0 and 2.0 models

--- ml-models/LSTM-FFNN-GRU-TCN/rmse.py
+++
@@ -1,5 +0,0 @@
-from sklearn.metrics import mean_squared_error
-import numpy as np
-
-def calculate_rmse(predictions, ground_truth):
-    return np.sqrt(mean_squared_error(ground_truth, predictions))
